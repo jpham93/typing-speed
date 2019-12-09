39ab907bdd27a848376eb7f01e871ee7437bd19e
James Pham <jamespham93@yahoo.com>
2019-12-09 10:52:59 -0800

    Add timing functionality

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
   const [input, setInput] = useState('');
+  const [timeLeft, setTime] = useState(5);
 
   const handleChange = e => {
     const {value} = e.target;
@@ -15,6 +16,12 @@ const App = () => {
     return wordsArr.filter(word => word !== "").length;
   };
 
+  useEffect(() => {
+    setTimeout(() =>
+      timeLeft > 0 && setTime(prevTime => prevTime - 1), 1000
+    );
+  }, [timeLeft]);
+
   return (
     <>
       <form>
@@ -24,7 +31,7 @@ const App = () => {
           value={input}
           onChange={handleChange}
         />
-        <h4>Time Remaining: </h4>
+        <h4>Time Remaining: {timeLeft}</h4>
         <button>Start</button>
         <h1>Word count: </h1>
       </form>
