8743075e2748e358bf67f9aa2915ff9d0f4048cc
James Pham <jamespham93@yahoo.com>
2019-12-09 10:28:15 -0800

    Added state to track user input

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [input, setInput] = useState('');
+
+  const handleChange = e => {
+    const {value} = e.target;
+    setInput(value);
+
+    console.log(input);
+  };
+
   return (
     <>
-      <h1>Speed Typer</h1>
-      <textarea/>
-      <h4>Time Remaining: </h4>
-      <button>Start</button>
-      <h1>Word count: </h1>
+      <form>
+        <h1>Speed Typer</h1>
+        <textarea
+          name='input'
+          value={input}
+          onChange={handleChange}
+        />
+        <h4>Time Remaining: </h4>
+        <button>Start</button>
+        <h1>Word count: </h1>
+      </form>
     </>
   );
 };
